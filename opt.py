caac6e95642c09b1e1277c78116ce7a1a2b7cef2
Robel kebede <robelkebede44@gmail.com>
2020-09-07 11:35:41 +0300

    init commit

---
+++ opt.py
@@ -0,0 +1,89 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.datasets import load_iris
+import pdb
+
+inp = 4; hidden = 15; out=4
+rate = 0.03
+
+w1 = np.random.uniform(-1,1,(inp,hidden))
+w2 = np.random.uniform(-1,1,(hidden,out))
+
+iris = load_iris()
+
+def one_hot(x,nb_classes):
+
+    zeros = np.zeros(nb_classes)
+    zeros[x] = 1
+    return zeros
+
+
+X = iris.data
+#y = iris.target
+y = [list(one_hot(i,4)) for i in iris.target]
+
+#pdb.set_trace()
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def feedforward(x,w1,w2):
+
+    z = np.tanh(x@w1)
+    yh = sigmoid(z@w2)
+    return yh
+
+def mutate(w,rate):
+
+    dw = np.random.uniform(-1,1,(w.shape)) * rate
+    return dw + w
+
+
+def train(x,y,w1,w2,rate,gen):
+
+    err = []
+
+    for i in range(gen):
+        
+        
+        mw1 = mutate(w1,rate)
+        mw2 = mutate(w2,rate)
+
+        ff1 = feedforward(x,w1,w2) #parent
+        ff2 = feedforward(x,mw1,mw2) #child
+
+        error_parent = np.sum(np.abs(y - ff1)) #parent cost
+        error_child = np.sum(np.abs(y - ff2)) #child cost
+
+        if error_child<error_parent:
+            
+            #update child error
+            w1 = mw1 
+            w2 = mw2
+            
+        print("Training Error ",error_child)  
+        err.append(error_child)
+
+    return w1,w2,err
+
+def main():
+
+    tw1,tw2,err = train(X,y,w1,w2,0.03,1000)
+
+    res = np.tanh(X[0] @ w1)
+    res = res @ w2
+
+    print(res,y[0])
+
+    plt.plot(err)
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    main()
+
+
+
+
